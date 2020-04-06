fd03c985b4d352c311af0a9342e679e836df97e7
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-06 14:48:42 -0600

    Server now echoes input to connections

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -8,8 +9,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from go-tcp.main!")
+	fmt.Println("Spinning TCP mux...")
 
+	// Initialize listener
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalln(err)
@@ -17,14 +19,32 @@ func main() {
 	defer listener.Close()
 
 	for {
+		// Accept connection requests
 		connection, err := listener.Accept()
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
-		io.WriteString(connection, "\nHello from TCP server!\n")
+		// Initial connection message
+		io.WriteString(connection, "\nHello from TCP server!  Type something in.\n\n")
 
-		connection.Close()
+		go handle(connection)
 	}
+}
+
+func handle(connection net.Conn) {
+	// Initialize scanner loop
+	scanner := bufio.NewScanner(connection)
+	for scanner.Scan() {
+		// Scan input into line
+		line := scanner.Text()
+
+		// Print response in mux log
+		fmt.Println("Got message:", line)
+
+		// Echo input to connection with formatting.
+		fmt.Fprintf(connection, "I heard you say: \"%s\"\n", line)
+	}
+	defer connection.Close()
 }
